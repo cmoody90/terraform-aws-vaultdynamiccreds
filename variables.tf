9304cb6fba0444776e387ce802add0c3a21c24d8
cmoody90 <119501907+cmoody90@users.noreply.github.com>
2024-05-08 19:15:18 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,19 +1,21 @@
 variable "aws_region" {
   description = "The AWS region to create things in."
-  default     = "us-west-2"
+      default = "us-west-2"
 }
 
 variable "db_name" {
   description = "The name of the database"
-  default     = ""
+      default = ""
 }
 
 variable "username" {
   description = "The username for database access"
-  default     = ""
+      default = ""
 }
 
 variable "password" {
   description = "The password for database access"
-  default     = ""
+      default = ""
 }
+
+
