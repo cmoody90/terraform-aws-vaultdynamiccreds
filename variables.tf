9d3720289e9efc7e121419bceda4a6d7de856861
cmoody90 <119501907+cmoody90@users.noreply.github.com>
2024-05-08 18:30:24 +0100

    Add files via upload

--- variables.tf
+++ variables.tf
@@ -2,3 +2,15 @@ variable "aws_region" {
   description = "The AWS region to create things in."
   default     = "us-west-2"
 }
+
+variable "db_name" {
+  description = "The name of the database"
+}
+
+variable "username" {
+  description = "The username for database access"
+}
+
+variable "password" {
+  description = "The password for database access"
+}
